8790a99a537ee6f5c79ed557997f505b2470ec0a
Niranjan <niranjaan.magare@gmail.com>
2020-12-27 10:25:11 +0530

    1. Remember me issue resolved. 2. Add employee code implemented with live sync.

--- app/src/main/java/com/example/dream_team/owner/activities/AddEmployeeScreen.java
+++ app/src/main/java/com/example/dream_team/owner/activities/AddEmployeeScreen.java
@@ -1,43 +1,56 @@
 package com.example.dream_team.owner.activities;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.dream_team.R;
+import com.example.dream_team.common_activities.COMMON;
 import com.example.dream_team.constants.Constant;
+import com.example.dream_team.modal_class.CONSTANTS;
 import com.example.dream_team.owner.Adapter.EmployeeAdapter;
+import com.example.dream_team.owner.Adapter.employeeObject;
 import com.example.dream_team.owner.dialog_fragments.AddUpdateEmployeeDialog;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class AddEmployeeScreen extends AppCompatActivity implements View.OnClickListener {
+    public final String TAG = "Dream_Team | AddEmpScrn";
     private RecyclerView employee_rv;
     FloatingActionButton addEmpFab;
     SearchView searchBar;
     ImageView edit;
     LinearLayoutManager linearLayoutManager;
     EmployeeAdapter employeeAdapter;
-    ArrayList<String> list = new ArrayList<>();
+    ArrayList<Object> list = new ArrayList<>();
+    private FirebaseFirestore db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_employee_screen);
         initialization();
-
     }
 
     private void initialization() {
+        db = FirebaseFirestore.getInstance();
 
-        list.add("Joy");
-        list.add("Flash");
-        list.add("Happy");
         employee_rv = findViewById(R.id.employee_rv);
         addEmpFab = findViewById(R.id.fab);
        // edit = findViewById(R.id.editEmployee);
@@ -45,16 +58,15 @@ public class AddEmployeeScreen extends AppCompatActivity implements View.OnClick
        // edit.setOnClickListener(this);
         addEmpFab.setOnClickListener(this);
         searchBar = findViewById(R.id.search_view);
+
         //seachbar
         searchBar.setQueryHint("Search...");
         searchBar.clearFocus();
         searchBar.onActionViewExpanded();
+
         //setting adapter
         linearLayoutManager = new LinearLayoutManager(this);
         employee_rv.setLayoutManager(linearLayoutManager);
-        employeeAdapter = new EmployeeAdapter(this, list);
-        employee_rv.setAdapter(employeeAdapter);
-
 
         searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -70,18 +82,52 @@ public class AddEmployeeScreen extends AppCompatActivity implements View.OnClick
         });
     }
 
-
-
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.fab:
                 AddUpdateEmployeeDialog addUpdateEmployeeDialog = AddUpdateEmployeeDialog.newInstance(null, Constant.ADD);
-                addUpdateEmployeeDialog.show(getSupportFragmentManager(), "Add employeee");
-
+                addUpdateEmployeeDialog.show(getSupportFragmentManager(), "Add employee");
         }
-
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
 
+        final String documentName = COMMON.getSharedData(CONSTANTS.document_name());
+
+        db.collection(CONSTANTS.whole_db()).document(documentName).collection(CONSTANTS.emp_cred())
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
+
+                        if (error != null) {
+                            Toast.makeText(AddEmployeeScreen.this, "Failed to fetch data from firestore", Toast.LENGTH_SHORT).show();
+                            Log.d(TAG, "onEvent: " + error.toString());
+                            return;
+                        }else if (!queryDocumentSnapshots.isEmpty()){
+                            ArrayList<Object> list2 = new ArrayList<>();
+
+                            for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                                Log.d(TAG, "onEvent: Data => " + document.getData());
+                                String name, type, number, status;
+
+                                name = document.getString(CONSTANTS.name());
+                                type = document.getString(CONSTANTS.type());
+                                number = document.getString(CONSTANTS.mobile());
+                                status = document.getString(CONSTANTS.status());
+
+                                if(!type.equals("OWNER")) list2.add(new employeeObject(name,type,number, status));
+                            }
+
+                            list = list2;
+                            employeeAdapter = new EmployeeAdapter(AddEmployeeScreen.this, list);
+                            employee_rv.setAdapter(employeeAdapter);
+                        }else {
+                            Log.d(TAG, "onEvent: Data not exists...");
+                        }
+                    }
+                });
+    }
 }
--- app/src/main/java/com/example/dream_team/owner/dialog_fragments/AddUpdateEmployeeDialog.java
+++ app/src/main/java/com/example/dream_team/owner/dialog_fragments/AddUpdateEmployeeDialog.java
@@ -1,6 +1,10 @@
 package com.example.dream_team.owner.dialog_fragments;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.service.voice.AlwaysOnHotwordDetector;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,28 +14,35 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Switch;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.dream_team.R;
+import com.example.dream_team.common_activities.DATABASE;
 import com.example.dream_team.constants.Constant;
+import com.example.dream_team.interfaces.CheckingNewInterface;
+import com.example.dream_team.modal_class.CONSTANTS;
+import com.example.dream_team.owner.activities.AddEmployeeScreen;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
-public class AddUpdateEmployeeDialog extends DialogFragment implements View.OnClickListener {
-    private TextView text;
+public class AddUpdateEmployeeDialog extends DialogFragment implements View.OnClickListener,AdapterView.OnItemSelectedListener {
+    private final String TAG = "Dream_Team";
     private EditText nameEditText, numberEditText;
     private TextInputLayout nameLayout, numberLayout;
     private Switch statusSwitch;
     private Button saveEmployee;
-    private Spinner spinner;
     private byte addUpdateStauts;
     private ArrayList list;
+    private String empName, empNumber, empStatus, empType;
+    private Spinner spinner;
+    private DATABASE database;
 
     public static AddUpdateEmployeeDialog newInstance(ArrayList list, byte flag) {
         AddUpdateEmployeeDialog addUpdateEmployeeDialog = new AddUpdateEmployeeDialog();
@@ -44,7 +55,6 @@ public class AddUpdateEmployeeDialog extends DialogFragment implements View.OnCl
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.add_update_employee_dialog, container);
-        text = view.findViewById(R.id.text);
         nameEditText = view.findViewById(R.id.emp_name);
         numberEditText = view.findViewById(R.id.emp_number);
         nameLayout = view.findViewById(R.id.input_layout_name);
@@ -52,29 +62,19 @@ public class AddUpdateEmployeeDialog extends DialogFragment implements View.OnCl
         statusSwitch = view.findViewById(R.id.statusSwitch);
         saveEmployee = view.findViewById(R.id.btn_save);
         saveEmployee.setOnClickListener(this);
-        spinner = view.findViewById(R.id.typeChoice);
-        ArrayAdapter arrayAdapter = ArrayAdapter.createFromResource(getContext(), R.array.typeChoice, R.layout.spinner_choose_color);
-        arrayAdapter.setDropDownViewResource(R.layout.spinner_text_color);
-        spinner.setAdapter(arrayAdapter);
-        spinner.setOnItemSelectedListener((new AdapterView.OnItemSelectedListener() {
-            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-                //get String here
-            }
-
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        }));
-
 
         //for update button
         if (Constant.UPDATE == addUpdateStauts) {
             saveEmployee.setText("Update");
-            text.setText("Update Details");
             saveEmployee.setOnClickListener(this);
             nameEditText.setText(list.toString());
             numberEditText.setText(list.toString());
         }
 
+        spinner = view.findViewById(R.id.typeChoice);
+        spinner.setOnItemSelectedListener(this);
+
+        database = new DATABASE();
         return view;
     }
 
@@ -82,11 +82,91 @@ public class AddUpdateEmployeeDialog extends DialogFragment implements View.OnCl
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_save:
-                if (Constant.ADD == addUpdateStauts)
-                    Toast.makeText(getContext(), "At add", Toast.LENGTH_SHORT).show();
-                else if (Constant.UPDATE == addUpdateStauts)
+                if (Constant.ADD == addUpdateStauts) {
+                    if (validateInputs()) {
+                        Log.d(TAG, "onClick: Name => " + nameEditText.getText().toString());
+                        Log.d(TAG, "onClick: Number => " + numberEditText.getText().toString());
+                        Log.d(TAG, "onClick: statusSwitch => " + statusSwitch.isChecked());
+                        Log.d(TAG, "onClick: Type => " + empType);
+
+                        Map<String, Object> empInfo = new HashMap<>();
+                        empInfo.put(CONSTANTS.name(), empName);
+                        empInfo.put(CONSTANTS.mobile(), empNumber);
+                        empInfo.put(CONSTANTS.status(), empStatus);
+                        empInfo.put(CONSTANTS.type(), empType);
+
+                        Log.d(TAG, "onClick: EMP Info => " + empInfo);
+
+                        database.addEmployee(empInfo, new CheckingNewInterface() {
+                            @Override
+                            public void callbackWithData(int result, Map<String, Object> data) {
+                                Log.d(TAG, "callbackWithData: Result => "+result);
+                                Log.d(TAG, "callbackWithData: Data => "+data);
+
+                                switch (result) {
+                                    case 0:
+                                        Log.d(TAG, "callbackWithData: Employee information inserted successfully => "+data);
+                                        dismiss();
+                                        break;
+                                    case 1:
+                                    case 2:
+                                        Log.e(TAG, "callbackWithData: Failed to insert employee information");
+                                        break;
+                                    default:
+                                        break;
+                                }
+
+                                if(data.containsKey(CONSTANTS.message()))
+                                    Toast.makeText(getActivity(), data.get(CONSTANTS.message()).toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                } else if (Constant.UPDATE == addUpdateStauts)
                     Toast.makeText(getContext(), "At update", Toast.LENGTH_SHORT).show();
                 break;
         }
+    }
+
+    private boolean validateInputs() {
+        if (nameEditText.getText().toString().trim().length() == 0) {
+            nameLayout.setError("Required Field");
+            nameLayout.setErrorTextColor(ColorStateList.valueOf(Color.RED));
+            nameLayout.setErrorEnabled(true);
+            return false;
+        } else if (numberEditText.getText().toString().trim().length() != 10) {
+            numberLayout.setError("Required Field");
+            numberLayout.setErrorTextColor(ColorStateList.valueOf(Color.RED));
+            numberLayout.setErrorEnabled(true);
+            return false;
+        } else {
+            empName = nameEditText.getText().toString().trim();
+            empNumber = numberEditText.getText().toString().trim();
+            if (statusSwitch.isChecked()) empStatus = "true";
+            else empStatus = "false";
+            return true;
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        switch (adapterView.getSelectedItem().toString()) {
+            case "Owner":
+                empType = "Owner";
+                break;
+            case "Waiter":
+                empType = "Waiter";
+                break;
+            case "Chef":
+                empType = "Chef";
+                break;
+            default:
+                empType = "";
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
